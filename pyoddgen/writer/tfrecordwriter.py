fc9b49e226745bc762547101c5dc687edd7e321b
engelmi <engel.michael@hotmail.de>
2019-07-24 13:53:15 +0200

    fixed writing .record files

--- pyoddgen/writer/tfrecordwriter.py
+++ pyoddgen/writer/tfrecordwriter.py
@@ -8,16 +8,17 @@ from pyoddgen.datastructures.gendatarecord import GeneratedDataRecord
 
 class TFRecordWriter(RecordWriter):
     """
-    Class to write TensorFlow Records to .record file.
+    Class to write generated data records to a TensorFlow .record file.
     """
 
-    def __init__(self, output_folder, record_type, record_name=""):
+    def __init__(self, output_folder, record_type, record_name="data"):
         """
         Constructor.
         :param output_folder: Output directory for the .record file.
         :param record_name: Output name for the .record file.
         """
         super(TFRecordWriter, self).__init__(output_folder, record_type)
+        self.record_name = record_name
         self.tf_writer = tf.python_io.TFRecordWriter(join(output_folder, record_name + '.record'))
 
     def write_data(self, record):
@@ -25,10 +26,9 @@ class TFRecordWriter(RecordWriter):
         Writes a record entry to the defined .record file.
         """
         if not isinstance(record, self.record_type):
-            raise Exception("Record parameter must be of type '" + str(type(GeneratedDataRecord)) +
-                            "'. Got '" + str(record) + "' instead. ")
+            raise Exception("Record parameter '" + str(record) + "' must be of type '" + str(type(GeneratedDataRecord)) + "'!")
         try:
-            self.tf_writer.write(record.to_tf_record()).SerializeToString()
+            self.tf_writer.write(record.to_tf_record().SerializeToString())
         except Exception as e:
             logging.error('Error occurred while writing tf record: ' + str(e))
 
